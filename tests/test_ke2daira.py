efb6f25655bb2bab27ce67f338de111653e71fe5
otariidae <otariidae@users.noreply.github.com>
2020-10-25 07:44:04 +0900

    add more tests

--- tests/test_ke2daira.py
+++ tests/test_ke2daira.py
@@ -7,3 +7,23 @@ def test_version():
 
 def test_matsudaira_ken():
     assert ke2dairanize("松平 健") == "ケツダイラ マン"
+
+
+def test_paul_maccartney():
+    assert ke2dairanize("ポール マッカートニー") == "マール ポッカートニー"
+
+
+def test_che_guevara():
+    assert ke2dairanize("チェ ゲバラ") == "ゲ チェバラ"
+
+
+def test_katou_ai():
+    assert ke2dairanize("加藤 あい") == "アトウ カイ"
+
+
+def test_atou_kai():
+    assert ke2dairanize("阿藤 快") == "カトウ アイ"
+
+
+def test_harry_potter():
+    assert ke2dairanize("ハリー ジェームズ ポッター") == "ポリー ジェームズ ハッター"
