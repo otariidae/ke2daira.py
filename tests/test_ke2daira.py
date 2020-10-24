83c49ccb38b88b463c346c9c6de05d59f05cc0d7
otariidae <otariidae@users.noreply.github.com>
2020-10-25 06:30:34 +0900

    first commit

---
+++ tests/test_ke2daira.py
@@ -0,0 +1,9 @@
+from ke2daira import __version__, ke2dairanize
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+def test_matsudaira_ken():
+    assert ke2dairanize("松平 健") == "ケツダイラ マン"
