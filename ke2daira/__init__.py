3215ca3a5de784652fc90bf44d22f5b894a6c49a
otariidae <otariidae@users.noreply.github.com>
2020-10-25 09:06:51 +0900

    0.1.2

--- ke2daira/__init__.py
+++ ke2daira/__init__.py
@@ -1,3 +1,3 @@
 from .main import ke2dairanize
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
--- tests/test_ke2daira.py
+++ tests/test_ke2daira.py
@@ -2,7 +2,7 @@ from ke2daira import __version__, ke2dairanize
 
 
 def test_version():
-    assert __version__ == "0.1.1"
+    assert __version__ == "0.1.2"
 
 
 def test_matsudaira_ken():
