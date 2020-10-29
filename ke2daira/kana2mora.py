92298f3020359dd0428704ebec6726e76aad9794
otariidae <otariidae@users.noreply.github.com>
2020-10-30 08:47:20 +0900

    add missing type annotation

--- ke2daira/kana2mora.py
+++ ke2daira/kana2mora.py
@@ -7,7 +7,7 @@ SUTEKANA = {"ァ", "ィ", "ゥ", "ェ", "ォ", "ャ", "ュ", "ョ", "ヮ"}
 
 def katakana2mora(kana: str) -> List[str]:
     chars = list(kana)
-    moras = []
+    moras: List[str] = []
     for char in chars:
         if char in SUTEKANA:
             previous_char = "" if len(moras) == 0 else moras.pop()
